17cae17fdbdc5c1b452dd0f80e9c1ebb681d5bc6
ItsLiang62 <andersonwlx62@gmail.com>
2026-03-17 11:51:47 +0800

    Create ImageDataset

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+
+class ImageDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        # Image classes = Names of subfolders in root image folder
+        self.classes = sorted([d for d in os.listdir(root_dir)
+                               if os.path.isdir(os.path.join(root_dir, d))])
+
+        # Map classes to numbers for easy labeling
+        self.class_to_idx = {class_name: idx for idx, class_name in enumerate(self.classes)}
+
+        # Raw input images and their labels
+        self.images = []
+        self.image_to_label = {}
+
+        for class_name in self.classes:
+            class_dir = os.path.join(root_dir, class_name)
+            label = self.class_to_idx[class_name]
+
+            for image_name in os.listdir(class_dir):
+                # Accept only selected image file types
+                if image_name.lower().endswith(('.png', '.jpg', '.jpeg')):
+                    image_path = os.path.join(root_dir, class_dir, image_name)
+                    self.images.append(image_path)
+                    self.image_to_label[image_path] = label
+
+    def __len__(self):
+        return len(self.image_to_label)
+
+    def __getitem__(self, idx):
+        image_path = self.images[idx]
+        label = self.image_to_label[image_path]
+
+        image = Image.open(image_path)
+        if image.mode != 'RGB':
+            image = image.convert('RGB')
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
